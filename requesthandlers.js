5f4cb2ee37bd9faba13b19f251392557979c3080
Jelmer Tiete <jelmer@tiete.be>
2014-04-11 06:18:02 +0200

    added license in all files, and readme

--- requesthandlers.js
+++ requesthandlers.js
@@ -1,3 +1,31 @@
+/*  Copyright (c) 2014, Jelmer Tiete.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote
+ *     products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 const url = require('url');
 
 function core(response,request) {
@@ -51,7 +79,7 @@ function rd(response,request) {
   switch (request.method){
     case 'POST':
       if(parameter.ep){ //end point - mandatory
-        var endpoint = parameter.ep;
+        var endpoint = parameter.ep; //TODO check length and input
         if(parameter.d){ //domain - optional
             
           }
@@ -85,6 +113,7 @@ function rdgroup(response, request) {
 
 function rdlookup(response) {
   console.log("Request handler 'rdlookup' was called.");
+
     response.statusCode = '205';
     response.end('205');
 }
